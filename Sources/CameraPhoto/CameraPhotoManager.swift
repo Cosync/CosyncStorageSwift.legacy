78f8d2b0bfd32505f5a42cde0c3fa854088a485c
Tola Voeung <tolacambo@gmail.com>
2022-09-27 11:01:21 +0700

    updated Camera

--- Sources/CameraPhoto/CameraPhotoManager.swift
+++ Sources/CameraPhoto/CameraPhotoManager.swift
@@ -4,27 +4,28 @@
 //
 //  Created by Tola Voeung on 26/9/22.
 //
-
+import PhotosUI
 import SwiftUI
 import UIKit
 
 @available(iOS 15.0, *)
 public struct CameraPhotoManager: UIViewControllerRepresentable {
     
-    
+    @Binding var pickerResult: [String]
     @Binding var selectedImage:UIImage?
     @Binding var isPresented: Bool
     
     private var sourceType: UIImagePickerController.SourceType = .camera
     
-    public init(selectedImage: Binding<UIImage?>, isPresented: Binding<Bool>) {
+    public init(pickerResult:Binding<[String]>, selectedImage: Binding<UIImage?>, isPresented: Binding<Bool>) {
+        self._pickerResult = pickerResult
         self._selectedImage = selectedImage
         self._isPresented = isPresented
         
     }
     
     public func makeCoordinator() -> ImagePickerViewCoordinator {
-        return ImagePickerViewCoordinator(selectedImage: $selectedImage, isPresented: $isPresented)
+        return ImagePickerViewCoordinator(imageIds:$pickerResult ,selectedImage: $selectedImage, isPresented: $isPresented)
     }
     
     public func makeUIViewController(context: Context) -> UIImagePickerController {
@@ -40,16 +41,20 @@ public struct CameraPhotoManager: UIViewControllerRepresentable {
     public func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
         // Nothing to update here
     }
+    
+   
 
 }
 
 @available(iOS 15.0, *)
 public class ImagePickerViewCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
+    @Binding var imageIds: [String]
     @Binding var selectedImage: UIImage?
     @Binding var isPresented: Bool
     
-    public init(selectedImage: Binding<UIImage?>, isPresented: Binding<Bool>) {
+    public init(imageIds:Binding<[String]>, selectedImage: Binding<UIImage?>, isPresented: Binding<Bool>) {
+        self._imageIds = imageIds
         self._selectedImage = selectedImage
         self._isPresented = isPresented
     }
@@ -57,12 +62,38 @@ public class ImagePickerViewCoordinator: NSObject, UINavigationControllerDelegat
     public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             self.selectedImage = image
+            saveImageToCameraRoll(inputImage: image)
         }
         self.isPresented = false
     }
     
     public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.isPresented = false
+    }
+    
+    public func saveImageToCameraRoll(inputImage:UIImage){
+        let imageSaver = ImageSaver()
+        
+        imageSaver.successHandler = {
+            print("Success!")
+            let fetchOptions = PHFetchOptions()
+            fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+            fetchOptions.fetchLimit = 1
+            let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+            
+            if let phAsset = fetchResult.firstObject {
+                self.imageIds.append(phAsset.localIdentifier)
+            }
+            
+        }
+
+        imageSaver.errorHandler = {
+            print("Oops: \($0.localizedDescription)")
+           
+        }
+        
+        imageSaver.writeToPhotoAlbum(image: inputImage)
+        
     }
     
 }
---
+++ Sources/CameraPhoto/ImageSaver.swift
@@ -0,0 +1,27 @@
+//
+//  ImageSaver.swift
+//  
+//
+//  Created by Tola Voeung on 27/9/22.
+//
+
+import Foundation
+import SwiftUI
+
+class ImageSaver: NSObject {
+    
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
